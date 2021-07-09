64122ca01f0c6b8ea65632187f451be9ad50cc46
Miguel Angel Baquero <migue_uio93@hotmail.com>
2021-07-09 01:08:47 -0500

    Second task and challenge completed

--- descuentos.js
+++ descuentos.js
@@ -1,5 +1,21 @@
-const precioOriginal = 120;
-const descuento = 18;
+const cupones = [
+  { cupon: "Liquidacion", descuento: 30 },
+  { cupon: "VIP", descuento: 25 },
+  { cupon: "Preferencial", descuento: 20 },
+  { cupon: "Cliente frecuente", descuento: 15 },
+  { cupon: "Cliente normal", descuento: 0 },
+];
+
+definirLista(cupones);
+
+function definirLista(listaCupones) {
+  let html = `<option value="0">--</option>`;
+  listaCupones.forEach((element) => {
+    const listaSeleccion = document.getElementById("SelectDiscount");
+    html += `<option value="${element.descuento}"> ${element.cupon} </option>`;
+    listaSeleccion.innerHTML = html;
+  });
+}
 
 function calcularPrecioConDescuento(precio, descuento) {
   const precioConDescuento = precio - (precio * descuento) / 100;
@@ -10,7 +26,8 @@ function calcularPrecioConDescuento(precio, descuento) {
 function activarCalculoDescuento() {
   const inputPrice = parseFloat(document.getElementById("InputPrice").value);
   const inputDiscount = parseFloat(
-    document.getElementById("InputDiscount").value
+    document.getElementById("SelectDiscount").value
   );
+  console.info(inputDiscount);
   calcularPrecioConDescuento(inputPrice, inputDiscount);
 }
